4430433104aca73678355d687da71a988f55e435
Top <wangchen@wangchendeMacBook-Pro.local>
2025-06-08 18:47:36 +0800

    add deploy script

---
+++ smart_contracts/scripts/deploy.ts
@@ -0,0 +1,28 @@
+// import { ethers } from "ethers";
+import { ethers } from "hardhat";
+
+
+async function main() {
+
+    const ContractFactory = await ethers.getContractFactory("Personality");
+    console.log("Deploying contract...");
+    const contract = await ContractFactory.deploy();
+    await contract.waitForDeployment();
+
+    const Account = await ethers.deployContract("ERC6551Account");
+    const account = await Account.waitForDeployment();
+
+    const Registry = await ethers.deployContract("ERC6551Registry");
+    const registry = await Registry.waitForDeployment();
+
+    console.log("Pinnie contract deployed at:", await contract.getAddress());
+    console.log("Account contract deployed at:", await account.getAddress());
+    console.log("Registry contract deployed at:", await registry.getAddress());
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
