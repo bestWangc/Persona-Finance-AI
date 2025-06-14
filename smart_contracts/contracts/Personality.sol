9fa412ab78288238f07541709cc48063417c79fe
Top <wangchen@wangchendeMacBook-Pro.local>
2025-06-14 18:41:52 +0800

    add nft create

--- smart_contracts/contracts/Personality.sol
+++ smart_contracts/contracts/Personality.sol
@@ -9,6 +9,20 @@ contract Personality is ERC721, ERC721URIStorage, Ownable {
     uint256 private _nextTokenId;
     string private _baseTokenURI;
 
+    struct PersonalInfo {
+        string riskPreference; 
+        string tradingStyle; 
+        uint256 initialCapital;
+        uint256 score;
+        address owner;
+        bool isForRent;
+        uint256 rentPricePerHour;
+    }
+    mapping(uint256 => PersonalInfo) public personalities;
+
+    event NFTCreated(uint256 indexed tokenId, address owner, string riskPreference, string tradingStyle);
+    event NFTForRent(uint256 indexed tokenId, uint256 rentPricePerHour);
+
     constructor(
         string memory baseURI
     ) ERC721("Personality Finance AI", "PFAI") Ownable(msg.sender) {
@@ -49,5 +63,30 @@ contract Personality is ERC721, ERC721URIStorage, Ownable {
         bytes4 interfaceId
     ) public view override(ERC721, ERC721URIStorage) returns (bool) {
         return super.supportsInterface(interfaceId);
+    }
+
+    function createNFT(address owner, string memory _riskPreference, string memory _tradingStyle, uint256 _initialCapital) external returns (uint256) {
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+        _mint(owner, newTokenId);
+        personalities[newTokenId] = Personality({
+            riskPreference: _riskPreference,
+            tradingStyle: _tradingStyle,
+            initialCapital: _initialCapital,
+            score: 100,
+            owner: owner,
+            isForRent: false,
+            rentPricePerHour: 0
+        });
+
+        emit NFTCreated(newTokenId, owner, _riskPreference, _tradingStyle);
+        return newTokenId;
+    }
+
+    function setRent(uint256 tokenId, bool _isForRent, uint256 _rentPricePerHour) external {
+        require(ownerOf(tokenId) == msg.sender, "Only owner can set rent");
+        personalities[tokenId].isForRent = _isForRent;
+        personalities[tokenId].rentPricePerHour = _rentPricePerHour;
+        emit NFTForRent(tokenId, _rentPricePerHour);
     }
 }
